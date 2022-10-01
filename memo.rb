03ee52463005e13d18d4a0dc45314acb5b372b9e
EC2 Default User <ec2-user@ip-172-31-15-10.ap-northeast-1.compute.internal>
2022-10-01 08:30:43 +0000

    rb-1

--- memo.rb
+++ memo.rb
@@ -1,36 +1,77 @@
-require "csv"
+# 実行手順
+# 1.「cd memo」と入力
+# 2.「ruby memo.rb」と入力
 
-puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+# CSVファイルを扱うためのライブラリを読み込んでいます
+require "csv" 
+ 
+ puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
 
-memo_type = gets.to_s.chomp
+ 
+ # ユーザーの入力値を取得し、数字へ変換しています
+ memo_type = gets.to_s 
+ puts "選択：" + memo_type
+ input_number = memo_type.chomp
+ 
+ #1を押したとき
+ if  input_number == "1"
+ puts"新規ファイルを作成します。ファイル名を記入してください。"
 
-if memo_type == "1"
-    puts "ファイルを入力(拡張子を除く)"
-    file_name = gets.to_s.chomp
-    puts "メモ内容を入力し、enterキーを入力して下さい"
-    puts "完了したらCtrl+Dを入力して下さい"
-    file_contents = readlines.map(&:chomp) 
-    CSV.open("#{file_name}.csv","w") do |csv|
-    csv << [file_contents]      
-    end
-elsif memo_type == "2"
-    puts "1(ファイルの参照) 2(ファイルへの書き込み)"   
-    file_edit = gets.to_s.chomp
-    if file_edit == "1"
-        puts "ファイル名を入力(拡張子を除く)"
-        file_name = gets.to_s.chomp
-        puts CSV.read("#{file_name}.csv")
-    elsif  file_edit == "2"
-        puts "ファイル名を入力(拡張子を除く)"
-        file_name = gets.to_s.chomp
-        puts "メモ内容"
-        file_contents = gets.to_s
-        CSV.open("#{file_name}.csv","a") do |csv|
-        csv << [file_contents]
-        end
-    else
-        puts "エラー"
-    end    
-else
-    puts "エラー"
+ 
+ file_name_new = gets.chomp
+ puts "タイトル：" + file_name_new
+ 
+ puts"メモの内容を記入して下さい。入力後はCtrl + D で保存"
+
+ #参考サイト　https://qiita.com/ayaka-k/questions/bb8ff2193f8f9dd1f071
+ imput_memo = STDIN.read
+ memo = imput_memo.chomp
+
+ # 保存ができているか確認をするためのコード
+ # puts "#{memo}"
+ 
+ CSV.open("#{file_name_new}.csv","w") do |csv|
+ csv.puts ["#{memo}"]
+ 
 end
+
+puts "保存したので終了致します。"
+
+ 
+ #2を押したとき
+ elsif  input_number == "2"
+
+ puts"既存のファイルを編集します。"
+ puts"ファイル名を入力してください。"
+
+ file_name = gets.chomp
+ 
+ begin
+
+ puts"指定したファイルの編集を開始します。"
+
+ 
+ file = File.open("#{file_name}.csv", "a")
+ 
+ imput_memo = gets.chomp
+ memo = imput_memo.chomp
+ 
+ file.write("#{memo}")
+ file.close
+ 
+ 
+ puts"編集を保存して終了します。"
+ 
+ rescue
+ 
+ puts"該当するファイルが特定できません。処理を終了します。"
+
+ end
+ 
+ puts "終了"
+ 
+ end
+
+ # SCVについてhttps://style.potepan.com/articles/27115.html
+ # gets メソッドについて　https://pikawaka.com/ruby/gets
+ #参考サイト：https://dosezen.com/2020/04/13/skillhacks-ruby-memo-app/
